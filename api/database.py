509df1c9791ba5f25c0e580caaf1425b6343a307
Bernarr-77 <Bernarrnarciso@outlook.com>
2026-03-06 16:30:19 -0300

    gets funcionando com tratamento de erros

--- api/database.py
+++ api/database.py
@@ -22,17 +22,39 @@ def start_bank():
     con.close()
 
 def save_lead(name, email, phone, status, created_at):
-    con = sqlite3.connect("leads.db")
-    cur = con.cursor()
-    sql = """
-    INSERT INTO leads(name, email, phone, status, created_at)
-    VALUES (?, ?, ?, ?, ?)
-    """
-    cur.execute(sql, (name, email, phone, status, created_at))
+    with sqlite3.connect("leads.db") as con:
+        cur = con.cursor()
+        sql = """
+        INSERT INTO leads(name, email, phone, status, created_at)
+        VALUES (?, ?, ?, ?, ?)
+        """
+        cur.execute(sql, (name, email, phone, status, created_at))
+
+def get_lead_id(lead_id):
+    with sqlite3.connect("leads.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        sql = """
+        SELECT * FROM leads WHERE id = ?
+        """
+        cur.execute(sql, (lead_id,))
+        row  = cur.fetchone()
+        if row:
+            return dict(row)
+        return None
     
-    con.commit()
-     
-    con.close()
+def get_lead_email(lead_mail, lead_phone):
+    with sqlite3.connect("leads.db") as con:
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        sql = """
+        SELECT * FROM leads WHERE email = ? OR phone = ? 
+        """
+        cur.execute(sql, (lead_mail, lead_phone,))
+        row  = cur.fetchone()
+        if row:
+            return dict(row)
+        return None
 
 if __name__ == "__main__":
     start_bank()
--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from schemas import LeadInput, LeadsProcess
-from database import start_bank,save_lead
+from database import save_lead, get_lead_id,get_lead_email
+import sqlite3
+from pydantic import EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,6 +13,38 @@ def post_leads(payload: LeadInput):
 
     lead_complete = LeadsProcess(**payload.model_dump())
     data_for_save = lead_complete.model_dump(exclude={"id"}, mode="json")
-    save_lead(**data_for_save)
+    try:
+        save_lead(**data_for_save)
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=409, detail= "Email ou telefone ja estão cadastrados no sistema, tente novamente")
+    except Exception as error_500:
+        raise HTTPException(status_code=500, detail= f"Erro desconhecido: {str(error_500)}")
+    return{"Message": "Criado com sucesso", "dados": lead_complete}
 
-    return{"Mensage": "Criado com sucesso", "dados": lead_complete}
+@app.get("/leads/search")
+def get_clients_mail(Lead_mail: Optional[EmailStr] = None, lead_phone: Optional[PhoneNumber] = None):
+
+    if Lead_mail is None and lead_phone is None:
+        raise HTTPException(status_code=400, detail= "Você precisa mandar pelo menos um dado para fazer a busca ")
+    try:
+        get_leads = get_lead_email(Lead_mail, lead_phone)
+
+    except Exception as error_500:
+        raise HTTPException(status_code=500, detail= f"Erro desconhecido: {str(error_500)}")
+    
+    if get_leads is None:
+        raise HTTPException(status_code=400, detail= "Não existe ninguem com esse email ou telefone ")
+    return {"Message": "Encontrado com sucesso!", "dados": get_leads}
+
+@app.get("/leads/{lead_id}")
+def get_clients_id(lead_id: int):
+    try:
+        lead_get = get_lead_id(lead_id)
+    except Exception as error_500:
+        raise HTTPException(status_code=500, detail= f"Erro desconhecido: {str(error_500)}")
+    
+    if lead_get is None:
+        raise HTTPException(status_code=404, detail= "Não existe ninguem com esse ID")
+
+    return {"Message": "Encontrado com sucesso!", "dados": lead_get}
+
