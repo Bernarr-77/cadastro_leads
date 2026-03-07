c863a0d2a5a4eb1423a7e00094963fcd547b4475
Bernardo Andrade <bernarrnarciso@outlook.com>
2026-03-07 17:14:35 -0300

    deixando mais clean

--- teste_velocidade.py
+++ teste_velocidade.py
@@ -5,17 +5,14 @@ def testar_performance():
     con = sqlite3.connect("leads.db")
     cur = con.cursor()
 
-    print("Iniciando 1.000 buscas completas no banco de dados...")
+    print("Iniciando 1.000 buscas no banco de dados")
     
-    # Marca o tempo inicial no relógio do processador
     inicio = time.time()
     
-    # Fazemos o banco procurar um nome que nem existe 1.000 vezes seguidas
     for _ in range(1000):
         cur.execute("SELECT * FROM leads WHERE name = 'Bernardo Inexistente'")
         cur.fetchall()
         
-    # Marca o tempo final
     fim = time.time()
     
     tempo_total = fim - inicio
